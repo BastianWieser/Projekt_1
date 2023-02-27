a3c39ef77fcfb7de5f1b8a37b132a4c0a0a90c7c
KadrT <ktas@tsn.at>
2023-02-27 19:51:30 +0100

    calculater_gui_func.py
    Erweiterung der GUI mit den benötigten Funktionen um einfache arithmetische Rechnungen und complexe zahlen zu rechnen.
    +Erweiterung der GUI mit Komma, imaginäres j
    +Funktion die hinzugefügt wurden:
    - result_calc()

--- calculator_gui.py
+++ calculator_gui.py
@@ -5,7 +5,16 @@ abhängig von den zusätzlichen Rechenfunktionen.
 Es sind keine Rechenfunktionen momentan vorhanden. Nur eine optische GUI
 by tobias
 26.02.2023
+
+Branch von der GUI
+Erweiterung der GUI mit den benötigten Funktionen um einfache arithmetische Rechnungen und complexe zahlen zu rechnen
++Erweiterung der GUI mit Komma, imaginäres j 
++Funktion die hinzugefügt wurden:
+- result_calc()
+by Kadir
+27.02.2023
 """
+import cmath
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton
 
@@ -35,8 +44,8 @@ class Calculator(QWidget):
         self.button_sub = QPushButton('-')
         self.button_mul = QPushButton('*')
         self.button_div = QPushButton('/')
-        self.button_empty1 = QPushButton(' ')
-        self.button_empty2 = QPushButton(' ')
+        self.button_kom = QPushButton('.')
+        self.button_img = QPushButton('j')
         self.button_result = QPushButton('=')
         self.button_clear = QPushButton('Clear')
 
@@ -65,9 +74,9 @@ class Calculator(QWidget):
         hbox3.addWidget(self.button_3)
         hbox3.addWidget(self.button_mul)
 
-        hbox4.addWidget(self.button_empty1)
+        hbox4.addWidget(self.button_kom)
         hbox4.addWidget(self.button_0)
-        hbox4.addWidget(self.button_empty2)
+        hbox4.addWidget(self.button_img)
         hbox4.addWidget(self.button_div)
 
         hbox5.addWidget(self.button_result)
@@ -97,8 +106,11 @@ class Calculator(QWidget):
         self.button_sub.clicked.connect(lambda: self.show_display('-'))
         self.button_mul.clicked.connect(lambda: self.show_display('*'))
         self.button_div.clicked.connect(lambda: self.show_display('/'))
+        self.button_kom.clicked.connect(lambda: self.show_display('.'))
+        self.button_img.clicked.connect(lambda: self.show_display('j'))
+
+        self.button_result.clicked.connect(self.result_calc)
         self.button_clear.clicked.connect(self.clear_display)
-        #button with the result function is needed here !!!
 
     #show number or function on button
     def show_display(self, text):
@@ -109,6 +121,26 @@ class Calculator(QWidget):
     #clear numbers, calculation
     def clear_display(self):
         self.result_display.setText('')
+
+    # calculate result and show on display
+    def result_calc(self):
+        try:
+            expr = self.result_display.text()
+            # replace 'j' with 'j' * 1j to create complex numbers
+            expr = expr.replace('j', 'j*1j')
+            # evaluate the expression using eval
+            result = eval(expr)
+            # check if the result is a complex number and format it accordingly
+            if isinstance(result, complex):
+                result = '{:.2f}'.format(result.real)
+            else:
+                result = str(result)
+            self.result_display.setText(result)
+        except (SyntaxError, ZeroDivisionError, TypeError):
+            self.result_display.setText('Error')
+
+
+
 
 #execute programm
 if(__name__ == '__main__'):
