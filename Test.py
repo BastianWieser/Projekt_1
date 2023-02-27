1daff672f250b347159da7b4c7b00f81a982b940
BastianWieser <basti007.wieser@gmail.com>
2023-02-27 20:50:43 +0100

    started the File I/O

--- Test.py
+++ Test.py
@@ -1,1 +1,7 @@
-print("Hello World")
+f = open("testfile.txt", "w")
+f.write("2+2=4")
+f.close()
+
+#open and read the file after the overwriting and creating:
+f = open("testfile.txt", "r")
+print(f.read())
